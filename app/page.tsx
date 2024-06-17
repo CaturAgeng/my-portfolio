13503eb4a85d5f3b7a2c796866a977fb51aae2d4
CaturAgeng <caturageng023@gmail.com>
2024-06-17 23:10:30 +0700

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
 'use client'
 
+import { useRef } from "react";
 import HeroSection from "@/components/HeroSection";
 import Features from "@/components/Features";
 import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
+  const heroRef = useRef<HTMLDivElement>(null);
+  const featuresRef = useRef<HTMLDivElement>(null);
+  const testimonialsRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = (sectionRef: React.RefObject<HTMLDivElement>) => {
+    if (sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <>
-      <HeroSection />
-      <Features />
-      <Testimonials />
+      <div ref={heroRef}>
+        <HeroSection scrollTo={() => scrollToSection(featuresRef)} />
+      </div>
+      <div ref={featuresRef}>
+        <Features scrollTo={() => scrollToSection(testimonialsRef)} />
+      </div>
+      <div ref={testimonialsRef}>
+        <Testimonials scrollTo={() => scrollToSection(heroRef)}/>
+      </div>
     </>
   );
 }
